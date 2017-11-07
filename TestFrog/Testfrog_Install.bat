6bc0c5c00315bd67d308248204ee809d66fcd1f0
amitbasak <amitbasak@infrrd.ai>
2017-11-07 23:20:36 +0530

    updating installation file

--- TestFrog/Testfrog_Install.bat
+++ TestFrog/Testfrog_Install.bat
@@ -1,3 +1,3 @@
-mvn install:install-file -Dfile="%cd%\bin\testfrog-0.0.1-SNAPSHOT-jar-with-dependencies.jar" -DgroupId="com.infrrd" -DartifactId=testfrog -Dversion="0.0.1-SNAPSHOT" -Dpackaging=jar
 setx DRIVER_HOME "%cd%\drivers"
+mvn install:install-file -Dfile="%cd%\bin\testfrog-0.0.1-SNAPSHOT-jar-with-dependencies.jar" -DgroupId="com.infrrd" -DartifactId=testfrog -Dversion="0.0.1-SNAPSHOT" -Dpackaging=jar
 pause
